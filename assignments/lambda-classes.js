6a99467a7415ba06d9e266576cf24f37d4df2a9c
JacobWellner83 <jcwellner83@gmail>
2019-04-04 22:21:04 -0500

    Lambda-Classes: Person Class created

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,13 @@
 // CODE here for your Lambda Classes
+//Person Class:
+class Person{
+    constructor(variables){
+        this.name = variables.name;
+        this.age = variables.age;
+        this.location = variable.location;
+        this.gender = variables.gender;
+    }
+    speak(){
+        return `Hello, my name is ${this.name}, I am from ${this.location}`
+    }
+}
